c65a540064dc23c153863841dd6ef0086e2d8c76
jamazi <31401744+jamazi@users.noreply.github.com>
2020-05-06 22:59:21 +0300

    added compact option

--- prayers.py
+++ prayers.py
@@ -19,6 +19,13 @@ prayers_arabic = {
     'Maghrib': 'المغرب',
     'Isha': 'العشاء'
 }
+prayers_arabic_compact = {
+    'Fajr': 'فجر',
+    'Dhuhr': 'ظهر',
+    'Asr': 'عصر',
+    'Maghrib': 'مغرب',
+    'Isha': 'عشا'
+}
 
 def update():
     try:
@@ -31,6 +38,7 @@ def update():
 
 def main(arguments):
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--compact', action='store_true', help='Show less text in output')
     parser.add_argument('--next', action='store_true', help='Show time remaining for next salah')
     parser.add_argument('--update', action='store_true', help='Force update of prayers file')
     args = parser.parse_args(arguments)
@@ -42,28 +50,31 @@ def main(arguments):
             prayers_info = json.load(f)
     
     if not prayers_info:
-        return
+        return -1
     
     prayers_date = datetime.fromtimestamp(int(prayers_info['data']['date']['timestamp'])).date()
     if prayers_date < datetime.today().date():
         prayers_info = update()
         if not prayers_info:
-            return
+            return -1
     
-    items = filter(lambda item: item[0] in prayers_arabic, prayers_info['data']['timings'].items())
-    items = [(prayers_arabic[item[0]], datetime.combine(prayers_date, datetime.strptime(item[1], '%H:%M').time())) for item in items]
+    items = filter(lambda item: item[0] in (prayers_arabic if not args.compact else prayers_arabic_compact), prayers_info['data']['timings'].items())
+    items = [(
+        prayers_arabic[item[0]] if not args.compact else prayers_arabic_compact[item[0]], 
+        datetime.combine(prayers_date, datetime.strptime(item[1], '%H:%M').time())
+        ) for item in items]
     items.append((items[0][0], items[0][1] + timedelta(days=1))) # add next day fajr
-    
+
     if args.next:
         now = datetime.now()
         next_prayers = list(filter(lambda item: item[1] > now, items))
         if next_prayers: 
             next_prayer = min(next_prayers, key=lambda item: abs(item[1] - now))
             time_remaining = next_prayer[1] - now
-            print(next_prayer[0], next_prayer[1].strftime('%-I:%M%P'), '\n', 'متبقي', ':'.join(str(time_remaining).split(':')[:2]))
+            print(next_prayer[0], next_prayer[1].strftime('%-I:%M%P' if not args.compact else '%-I:%M'), '(' + ('متبقي ' if not args.compact else '')  + ':'.join(str(time_remaining).split(':')[:2]) + ')')
     else:
-        for item in items:
-            print(item[0], item[1].strftime('%I:%M%P'))
+        for idx in range(len(items) - 1):
+            print(items[idx][0], items[idx][1].strftime('%-I:%M%P' if not args.compact else '%-I:%M'))
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
